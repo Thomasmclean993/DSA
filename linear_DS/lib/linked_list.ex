2c30fd83ff78d2ffdd5b4130f5dbfee835a165b1
Thomas McLean <tmclean@covermymeds.com>
2022-10-18 13:33:41 -0400

    complete queue

--- linear_DS/lib/linked_list.ex
+++ linear_DS/lib/linked_list.ex
@@ -9,12 +9,32 @@ defmodule LinkedList do
   6. check list for items
   """
 
-  def new_grocceries("empty"), do: []
-  def new_grocceries("full"), do: ["apples", "kale", "salmon", "ribeye"]
 
-  def get_first_item(list), do: hd(list)
-  def remove_first_item(list), do: tl(list)
-  def add_item(list, new_item), do: list ++ [new_item]
-  def count_items(list), do: length(list)
-  def check_grocceries(list, item), do: item in list
+  def new_grocceries("empty") do
+    []
+  end
+
+  def new_grocceries("full") do
+     ["apples", "kale", "salmon", "ribeye"]
+  end
+
+  def get_first_item(list) do
+     hd(list)
+  end
+
+  def remove_first_item(list) do
+     tl(list)
+  end
+
+  def add_item(list, new_item) do
+     list ++ [new_item]
+  end
+
+  def count_items(list) do
+    length(list)
+  end
+
+  def check_grocceries(list, item) do
+     item in list
+  end
 end
